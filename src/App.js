4521371adf045014ce3e2d0afa39f09931eb50c2
yamatatsu <the.kinnikuman@gmail.com>
2018-10-08 23:48:25 +0900

    機能落としてリニューアル

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React from 'react'
-import QrReader from './components/QrReader'
+import QrReader from 'react-qr-reader'
+import { Set } from 'react-powerplug'
 
-export default (props) => {
-  const { dispatch, textByQr } = props
-
-  const to = disp => () => dispatch('changeDisp', disp)
-
-  const backToLinks = to('Links')
-
-  switch (props.disp) {
-    case 'FileUploader': {
-      return <QrReader legacyMode {...{ textByQr, backToLinks, dispatch } } />
-    }
-    case 'QrReader': {
-      return <QrReader {...{ textByQr, backToLinks, dispatch }} />
-    }
-
-    default: {
-      return (
-        <ul>
-          <li>
-            <a href="#" onClick={to('QrReader')}>webRTC</a>
-          </li>
-          <li>
-            <a href="#" onClick={to('FileUploader')}>FileAPI</a>
-          </li>
-        </ul>
-      )
-    }
-  }
+export default function App(props) {
+  return (
+    <Set>
+      {({ values, remove, add }) => (
+        <div>
+          <div>
+            <QrReader
+              previewStyle={{ height: 240, width: 320 }}
+              handleError={err => console.log(err)}
+              handleScan={data => add(data)}
+            />
+          </div>
+          {values.map(text => (
+            <div id={text}>
+              <a href={text} target='_blank' rel="noopener noreferrer">{text}</a>
+            </div>
+          ))}
+        </div>
+      )}
+    </Set>
+  )
 }
--- src/index.js
+++ src/index.js
@@ -3,42 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-const initialState = {
-  disp: 'Links',
-  textByQr: null,
-}
-
-const actions = {
-  changeDisp: (state, disp) => ({ ...state, disp, textByQr: null }),
-  setTextByQr: (state, textByQr) => ({ ...state, textByQr })
-}
-
-const dispatcher = initDispatcher(actions)
-dispatcher(initialState)()
-
-function initDispatcher (actions) {
-  function dispatcher (state) {
-    return (actionName, ...args) => {
-      console.log(`dispatched actionName: ${actionName}, args: [${args.join(', ')}]`)
-      const newState = (actionName && actions[actionName]) ? actions[actionName](state, ...args) : state
-      render(newState, dispatcher)
-    }
-  }
-  return dispatcher
-}
-
-function render (state, dispatcher) {
-  let rootProps = { ...state, dispatch: dispatcher(state) }
-  if (module.hot) {
-    rootProps = (module.hot.data) ? module.hot.data.state : rootProps
-    module.hot.dispose(data => data.state = rootProps)
-  }
-  ReactDOM.render(
-    <App {...rootProps} />,
-    document.getElementById('root')
-  )
-}
-
-if (module.hot) {
-  module.hot.accept()
-}
+ReactDOM.render(<App/>, document.getElementById('root'))
